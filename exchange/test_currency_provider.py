07097e23a58b00696826fd09f8e6a5bc0d09fc2a
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2023-11-16 20:43:46 +0200

    Add unit test for mono provider

---
+++ exchange/test_currency_provider.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+
+import responses
+
+from exchange.currency_provider import MonoProvider, SellBuy
+
+
+def test_mono_currency_provider():
+    provider = MonoProvider("USD", "UAH")
+    rate_mocked = MagicMock(return_value=SellBuy(sell=27.0, buy=27.0))
+    provider.get_rate = rate_mocked
+    rate = provider.get_rate()
+    assert rate == SellBuy(sell=27.0, buy=27.0)
+    rate_mocked.assert_called()
+
+
+@responses.activate
+def test_mono_with_data():
+    responses.get("https://api.monobank.ua/bank/currency", json=[
+        {
+            "currencyCodeA": 840,
+            "currencyCodeB": 980,
+            "rateBuy": 28.0,
+            "rateSell": 28.0,
+        }
+    ])
+    provider = MonoProvider("USD", "UAH")
+    rate = provider.get_rate()
+    assert rate == SellBuy(sell=28.0, buy=28.0)
